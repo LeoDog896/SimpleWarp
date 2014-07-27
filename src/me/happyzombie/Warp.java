8ee4421e1b6992d39d2f7dcf38ff2bfc9af1fc8c
happyzombie707 <leake.lol.richard@gmail.com>
2014-07-27 18:04:39 +0100

    Warp class added
    Object to be used in the list. Contains warp name, world name, x, y and
    z.

---
+++ src/me/happyzombie/Warp.java
@@ -0,0 +1,44 @@
+package me.happyzombie;
+
+public class Warp {
+	
+	private String name;
+	private String world;
+	private int x;
+	private int y;
+	private int z;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getWorld() {
+		return world;
+	}
+	public void setWorld(String world) {
+		this.world = world;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getZ() {
+		return z;
+	}
+	public void setZ(int z) {
+		this.z = z;
+	}
+	
+	
+}
