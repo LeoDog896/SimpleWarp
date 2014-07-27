423b942d668c5aa2ee122d80fafc20b9975c846a
happyzombie707 <leake.lol.richard@gmail.com>
2014-07-27 18:01:48 +0100

    LoadWarp and Warp classes added
    Setting the plugin up for loading all the warps into a list, for faster
    access.

---
+++ src/me/happyzombie/LoadWarps.java
@@ -0,0 +1,12 @@
+package me.happyzombie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoadWarps {
+	
+	public static void load(){
+		List<Warp> warps = new ArrayList<Warp>();
+		
+	}
+}
