7d9d5b2ce648bc4468e7a3361110d1ea612c2145
happyzombie707 <leake.lol.richard@gmail.com>
2014-07-28 22:03:57 +0100

    Added CommandHandler to onEnable

--- src/me/happyzombie/WarpMain.java
+++ src/me/happyzombie/WarpMain.java
@@ -17,6 +17,8 @@ import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
 
+import Commands.CommandHandler;
+
 
 public class WarpMain extends JavaPlugin {
 	
@@ -33,6 +35,7 @@ public class WarpMain extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		getLogger().info("WarpMain 1.0 enabled!");
+		this.getCommand("test").setExecutor(new CommandHandler(this));
 		File WarpDir = new File ("plugins"+File.separator+"SimpleWarp"+File.separator+"Warps"+File.separator+getServer().getWorlds().get(0).getName()+File.separator);
 		if(!WarpDir.exists()){
 			WarpDir.mkdirs();
@@ -55,92 +58,5 @@ public class WarpMain extends JavaPlugin {
 		getLogger().info("Shutting down warps");
 	}
 	
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		new ListenerClass(this);
-		Player player = (Player) sender;
-		int len = 0;
-		
-		if (cmd.getName().equalsIgnoreCase("warp") && sender instanceof Player){
-			
-			if (args[0].equalsIgnoreCase("set")){
-				if (sender.hasPermission(warpSet)){
-					WarpCreate.createWarpFile(player, args);
-					return true;
-				}else{
-					sender.sendMessage(ChatColor.RED + "You do not have permission to do that.");
-					return true;
-				}
-			}else if (args[0].equalsIgnoreCase("help")){
-				player.sendMessage(ChatColor.GOLD +"This is the Simple Warp help!");
-				player.sendMessage(ChatColor.GOLD +"---------------------------");
-				player.sendMessage(ChatColor.GOLD +"/warp set [name] - creates a warp with the specified name.");
-				player.sendMessage(ChatColor.GOLD +"/warp delete [name] - deletes the warp with that name.");
-				player.sendMessage(ChatColor.GOLD +"/warp to [name] - takes you to the specified warp.");
-				player.sendMessage(ChatColor.GOLD +"/warp list - lists all warps in the current world.");
-				return true;
-			}else if (args[0].equalsIgnoreCase("to")){
-				WarpGoto.gotoWarp(player, args);
-				return true;
-			}else if (args[0].equalsIgnoreCase("test")){
-				LoadWarps.load(player, args);
-				return true;
-			}else if (args[0].equalsIgnoreCase("delete")){
-				if (sender.hasPermission(warpDelete)){
-					WarpDelete.deleteWarpFile(player, args);
-					return true;
-				}else{
-					sender.sendMessage(ChatColor.RED + "You do not have permission to do that.");
-					return true;
-				}
-			}else if (args[0].equalsIgnoreCase("list")){
-				if (sender.hasPermission(warpList)){
-					WarpList.listWarps(player, args);
-					return true;
-				}else{
-					sender.sendMessage(ChatColor.RED + "You do not have permission to do that.");
-					return true;
-				}
-			}else if (args[0].equalsIgnoreCase("beacon")){
-				if (sender.hasPermission(warpBeacon)){
-					player.sendMessage("coming soon!");
-					return true;
-				}else{
-					sender.sendMessage(ChatColor.RED + "You do not have permission to do that.");
-				}
-			}else if (args[0].equalsIgnoreCase("testcreate")){
-				CreateWarpTest.create(player, args);
-				return true;
-			}else if (args[0].equalsIgnoreCase("testdelete")){
-				DeleteWarpTest.delete(player, args);
-				return true;
-			}else if (args[0].equalsIgnoreCase("testgoto")){
-				GotoWarpTest.gotoWarp(player, args);
-				return true;
-			}else if (args[0].equalsIgnoreCase("getwarp")){
-				LoadWarps.getWarpName(player, args);
-				return true;
-			}else if (args[0].equalsIgnoreCase("testcompass")){
-				len = args.length;
-				if (len == 1){
-					WarpCompassTest.compassDefault(player, args);
-					return true;
-				}
-				WarpCompassTest.compassSet(player, args);
-				return true;
-		}else if (args[0].equalsIgnoreCase("compass")){
-					len = args.length;
-					if (len == 1){
-						WarpCompass.compassDefault(player, args);
-						return true;
-					}
-					WarpCompass.compassSet(player, args);
-					return true;
-			}else{
-				sender.sendMessage(ChatColor.RED + args[1] +" Is not a command, try /warp help for help.");
-				return false;
-			}
-		}
-		
-		return false;
-	}
+
 }
